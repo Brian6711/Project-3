ca3565166768f7cf37ff5d74e98a4f154199542e
Norma Garcia <misbendiciones@MacBook-Air-de-Norma.local>
2022-12-07 20:25:14 -0600

    final

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ api token = h1c8TUHe7wbXsiFWNP8rEJNtUKnyPNeVh7dDZjPL9dsfdy9pftQcNPDaAkTV
 
 
 let url = "https://soccer.sportmonks.com/api/v2.0/fixtures/date/" + year + month + day + "api_token=" + api_token + includes;
+
+
+
+
+window.addEventListener("load",function() { background(first_entry) });
--- launchpad.js
+++ launchpad.js
@@ -3,6 +3,14 @@ let device;
 
 
 
+function background(color) {
+    document.body.style.background = color;
+    first_entry = color
+    
+    
+}
+
+
 if (navigator.requestMIDIAccess){
     navigator.requestMIDIAccess().then(success, failure);
 }
@@ -55,24 +63,83 @@ if (velocity == 0){
 }
 
 
+function colors (){
+    if (first_entry = 'NaN'){
+        first_entry = Math.floor(Math.random() * 100); 
+    }
+    
+    if (first_entry < 0){
+        first_entry = 'black'
+    }
+
+    if (first_entry < 10 && first_entry >= 0 ){
+        first_entry = 'gray'
+    }
+
+
+    if (first_entry < 20 && first_entry >= 10 ){
+        first_entry = 'brown'
+    }
+
+    if (first_entry < 30 && first_entry >= 20 ){
+        first_entry = 'purple'
+    }
+
+
+    if (first_entry < 40 && first_entry >= 30 ){
+        first_entry = 'pink'
+    }
+
+    if (first_entry < 50 && first_entry >= 40 ){
+        first_entry = 'cyan'
+    }
+
+    if (first_entry < 60 && first_entry >= 50 ){
+        first_entry = 'yellow'
+    }
+
+    if (first_entry < 70 && first_entry >= 60 ){
+        first_entry = 'blue'
+    }
+
+
+    if (first_entry < 80 && first_entry >= 70 ){
+        first_entry = 'green'
+    }
+
+    if (first_entry < 90 && first_entry >= 80 ){
+        first_entry = 'red'
+    }
+
+
+    if (first_entry < 100 && first_entry >= 90 ){
+        first_entry = 'white'
+    }
+
+}
+
 function noteOn(note) {
     console.log(`note:${note} // on`)
     let newOBJ = document.createElement("div");
 
-if (note >= 52 && note <= 59){
+if (note >= 32 && note <= 67){
     colorM(note,3);
-    // newOBJ.style.backgroundColor = style.backgroundColor + 1;
+    first_entry = first_entry - 1
+    console.log(first_entry)
+    colors()
+    background(first_entry)
+
+
    
     
-  
-// const drawingArea = document.getElementById('drawingArea');
-// drawingArea.appendChild(newOBJ);
-
 }
 
-if (note >= 60 && note <= 67){
+if (note >= 68 && note <= 99){
     colorM(note,3);
-  
+    first_entry = first_entry + 1
+    console.log(first_entry)
+    colors()
+    background(first_entry)
 }
 
 
@@ -83,6 +150,7 @@ if (note >= 60 && note <= 67){
 function noteOff(note) {
     console.log(`note:${note} // off`)
 colorM(note, 21)
+//background(back)
     }
 
 
